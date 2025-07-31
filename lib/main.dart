587a71d2db8fab3fe5c44436853af997dfa8314c
Shalva Gagnidze <salvagagnidze@gmail.com>
2025-07-31 16:51:43 +0400

    Builded

--- lib/main.dart
+++ lib/main.dart
@@ -19,57 +19,53 @@ void main() async {
   //final isFirstLaunch = prefs.getBool('is_first_launch') ?? true;
   List<PageState> initialPages = [];
 
-  if (pagesDataString != null) {
-    try {
-      final pagesData = jsonDecode(pagesDataString) as List;
-      initialPages = await Future.wait(pagesData.map((pageData) async {
-        final page = PageState();
-        
-        // Restore all the values
-        page.selectedDate = DateTime.parse(pageData['selectedDate']);
-        page.selectedTime = TimeOfDay(
-          hour: pageData['selectedTime']['hour'],
-          minute: pageData['selectedTime']['minute']
-        );
-        
-        // Make sure to set the text values before adding listeners
-        page.passengerController = TextEditingController(
-          text: pageData['passengerControllerText'] ?? ''
-        );
-        page.onTheWayController = TextEditingController(
-          text: pageData['onTheWayControllerText'] ?? ''
-        );
-        page.freePassengersController = TextEditingController(
-          text: pageData['freePassengersControllerText'] ?? ''
-        );
-        
-        page.passengerCount = pageData['passengerCount'] ?? 0;
-        page.onlinePassengerCount = pageData['onlinePassengerCount'] ?? 0;
-        page.cashPassengerCount = pageData['cashPassengerCount'] ?? 0;
-        page.cardPassengerCount = pageData['cardPassengerCount'] ?? 0;
-        page.wizzPassengerCount = pageData['wizzPassengerCount'] ?? 0;
-        
-        page.showCashChildCounter = pageData['showCashChildCounter'] ?? false;
-        page.showCardChildCounter = pageData['showCardChildCounter'] ?? false;
-        page.cashChildPassengerCount = pageData['cashChildPassengerCount'] ?? 0;
-        page.cardChildPassengerCount = pageData['cardChildPassengerCount'] ?? 0;
-        
-        page.showOnTheWay = pageData['showOnTheWay'] ?? false;
-        page.onTheWayCashCount = pageData['onTheWayCashCount'] ?? 0;
-        
-        page.showFreePessangers = pageData['showFreePessangers'] ?? false;
-        page.freePassengersCount = pageData['freePassengersCount'] ?? 0;
-        
-        page.selectedTransferOptions = List<String>.from(pageData['selectedTransferOptions'] ?? []);
-        page.selectedSingleOption = pageData['selectedSingleOption'] ?? '';
-        page.selectedDriverName = pageData['selectedDriverName'] ?? '';
-        
-        return page;
-      }));
-    } catch (e) {
-      initialPages = [PageState()];
-    }
-  } else {
+  try {
+    final pagesData = jsonDecode(pagesDataString!) as List;
+    initialPages = await Future.wait(pagesData.map((pageData) async {
+      final page = PageState();
+      
+      // Restore all the values
+      page.selectedDate = DateTime.parse(pageData['selectedDate']);
+      page.selectedTime = TimeOfDay(
+        hour: pageData['selectedTime']['hour'],
+        minute: pageData['selectedTime']['minute']
+      );
+      
+      // Make sure to set the text values before adding listeners
+      page.passengerController = TextEditingController(
+        text: pageData['passengerControllerText'] ?? ''
+      );
+      page.onTheWayController = TextEditingController(
+        text: pageData['onTheWayControllerText'] ?? ''
+      );
+      page.freePassengersController = TextEditingController(
+        text: pageData['freePassengersControllerText'] ?? ''
+      );
+      
+      page.passengerCount = pageData['passengerCount'] ?? 0;
+      page.onlinePassengerCount = pageData['onlinePassengerCount'] ?? 0;
+      page.cashPassengerCount = pageData['cashPassengerCount'] ?? 0;
+      page.cardPassengerCount = pageData['cardPassengerCount'] ?? 0;
+      page.wizzPassengerCount = pageData['wizzPassengerCount'] ?? 0;
+      
+      page.showCashChildCounter = pageData['showCashChildCounter'] ?? false;
+      page.showCardChildCounter = pageData['showCardChildCounter'] ?? false;
+      page.cashChildPassengerCount = pageData['cashChildPassengerCount'] ?? 0;
+      page.cardChildPassengerCount = pageData['cardChildPassengerCount'] ?? 0;
+      
+      page.showOnTheWay = pageData['showOnTheWay'] ?? false;
+      page.onTheWayCashCount = pageData['onTheWayCashCount'] ?? 0;
+      
+      page.showFreePessangers = pageData['showFreePessangers'] ?? false;
+      page.freePassengersCount = pageData['freePassengersCount'] ?? 0;
+      
+      page.selectedTransferOptions = List<String>.from(pageData['selectedTransferOptions'] ?? []);
+      page.selectedSingleOption = pageData['selectedSingleOption'] ?? '';
+      page.selectedDriverName = pageData['selectedDriverName'] ?? '';
+      
+      return page;
+    }));
+  } catch (e) {
     initialPages = [PageState()];
   }
 
